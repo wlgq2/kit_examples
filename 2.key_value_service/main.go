a8ecad0b5b7ac3055bcc1054806185982c95dddc
wlgq2 <orcaer@yeah.net>
2019-05-14 22:25:33 +0800

    fix bug.

--- 2.key_value_service/main.go
+++ 2.key_value_service/main.go
@@ -1,81 +1,19 @@
 package main
 
-import "errors"
-import "github.com/go-kit/kit/endpoint"
-import "context"
-import httptransport "github.com/go-kit/kit/transport/http"
-import "net/http"
-import "time"
-import "fmt"
-import "encoding/json"
-import "bytes"
-import "io/ioutil"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
 
-type KeyValueService interface {
-	Set(string, string) (string, error)
-	Get(string) (string, error)
-}
-
-var ERROR_NOT_KEY = errors.New("can find this key.")
-
-type MapService struct {
-	datas map[string]string
-}
-
-func NewMapService() *MapService {
-	rst := MapService{
-		datas: make(map[string]string),
-	}
-	return &rst
-}
-
-func (service *MapService) Set(key string, value string) (string, error) {
-	service.datas[key] = value
-	return key, nil
-}
-
-func (service *MapService) Get(key string) (string, error) {
-	value, ok := service.datas[key]
-	if ok {
-		return value, nil
-	} else {
-		return value, ERROR_NOT_KEY
-	}
-
-}
-
-type KeyValueStruct struct {
-	Key   string `json:"key"`
-	Value string `json:"value"`
-}
-
-func makeSetEndpoint(service KeyValueService) endpoint.Endpoint {
-	return func(_ context.Context, request interface{}) (interface{}, error) {
-		req := request.(KeyValueStruct)
-		_, err := service.Set(req.Key, req.Value)
-		return req, err
-	}
-}
-
-func makeGetEndpoint(service KeyValueService) endpoint.Endpoint {
-	return func(_ context.Context, request interface{}) (interface{}, error) {
-		req := request.(KeyValueStruct)
-		return service.Get(req.Key)
-	}
-}
-
-func decodeStruct(_ context.Context, req *http.Request) (interface{}, error) {
-	var request KeyValueStruct
-	err := json.NewDecoder(req.Body).Decode(&request)
-	return request, err
-}
-
-func encodeStruct(_ context.Context, writer http.ResponseWriter, resp interface{}) error {
-	return json.NewEncoder(writer).Encode(resp)
-}
+	httptransport "github.com/go-kit/kit/transport/http"
+)
 
 func RunService() {
-	svc := NewMapService()
+	var svc KeyValueService
+	svc = NewMapService()
 
 	setHandler := httptransport.NewServer(
 		makeSetEndpoint(svc),
