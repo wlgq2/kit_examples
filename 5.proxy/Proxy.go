d28985350f5711c8d373a8973cdf2c6eda3d966a
wlgq2 <orcaer@yeah.net>
2019-05-17 21:45:42 +0800

    proxy

---
+++ 5.proxy/Proxy.go
@@ -0,0 +1,107 @@
+/*
+   Copyright 2019, orcaer@yeah.net  All rights reserved.
+   Author: orcaer@yeah.net
+   Last modified: 2019-5-17
+   Description: https://github.com/wlgq2/kit_examples
+*/
+
+package main
+
+import (
+	"context"
+	"net/url"
+	"time"
+
+	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/kit/sd"
+	"github.com/go-kit/kit/sd/lb"
+	httptransport "github.com/go-kit/kit/transport/http"
+)
+
+type KeyValueMiddleware func(KeyValueService) KeyValueService
+
+type KeyValueProxy struct {
+	ctx  context.Context
+	next KeyValueService
+	set  endpoint.Endpoint
+	get  endpoint.Endpoint
+}
+
+func NewProxy(ctx context.Context, urls []string) KeyValueMiddleware {
+
+	var maxAttempts = 3
+	var maxTime = 250 * time.Millisecond
+
+	var setEndpointer sd.FixedEndpointer
+	var getEndpointer sd.FixedEndpointer
+
+	for _, url := range urls {
+		setEndpoint := makeSetProxy(ctx, url)
+		setEndpointer = append(setEndpointer, setEndpoint)
+		getEndpoint := makeGetProxy(ctx, url)
+		getEndpointer = append(getEndpointer, getEndpoint)
+
+	}
+
+	balancerSet := lb.NewRoundRobin(setEndpointer)
+	balancerGet := lb.NewRoundRobin(getEndpointer)
+	retrySet := lb.Retry(maxAttempts, maxTime, balancerSet)
+	retryGet := lb.Retry(maxAttempts, maxTime, balancerGet)
+
+	return func(next KeyValueService) KeyValueService {
+		return &(KeyValueProxy{ctx, next, retrySet, retryGet})
+	}
+}
+
+func (service *KeyValueProxy) Set(key string, value string) (string, error) {
+	response, err := service.set(service.ctx, KeyValueStruct{Key: key, Value: value})
+	if err != nil {
+		return "", err
+	}
+
+	resp := response.(KeyValueStruct)
+
+	return resp.Key, nil
+}
+
+func (service *KeyValueProxy) Get(key string) (string, error) {
+	response, err := service.get(service.ctx, KeyValueStruct{Key: key, Value: ""})
+	if err != nil {
+		return "", err
+	}
+
+	resp := response.(KeyValueStruct)
+
+	return resp.Value, nil
+
+}
+
+func makeSetProxy(ctx context.Context, addr string) endpoint.Endpoint {
+	u, err := url.Parse(addr)
+	if err != nil {
+		panic(err)
+	}
+	u.Path = "/set"
+
+	return httptransport.NewClient(
+		"GET",
+		u,
+		encodeReq,
+		decodeResp,
+	).Endpoint()
+}
+
+func makeGetProxy(ctx context.Context, addr string) endpoint.Endpoint {
+	u, err := url.Parse(addr)
+	if err != nil {
+		panic(err)
+	}
+	u.Path = "/get"
+
+	return httptransport.NewClient(
+		"GET",
+		u,
+		encodeReq,
+		decodeResp,
+	).Endpoint()
+}
---
+++ 5.proxy/main.go
@@ -0,0 +1,109 @@
+/*
+   Copyright 2019, orcaer@yeah.net  All rights reserved.
+   Author: orcaer@yeah.net
+   Last modified: 2019-5-15
+   Description: https://github.com/wlgq2/kit_examples
+*/
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	httptransport "github.com/go-kit/kit/transport/http"
+)
+
+func RunService(port string) {
+	var svc KeyValueService
+	svc = NewMapService()
+	svc = NewLogMiddleware(svc)
+
+	setHandler := httptransport.NewServer(
+		makeSetEndpoint(svc),
+		decodeReq,
+		encodeResp,
+	)
+
+	getHandler := httptransport.NewServer(
+		makeGetEndpoint(svc),
+		decodeReq,
+		encodeResp,
+	)
+
+	mux := http.NewServeMux()
+	mux.Handle("/set", setHandler)
+	mux.Handle("/get", getHandler)
+	http.ListenAndServe(port, mux)
+}
+
+func RunClient(port string) {
+	time.Sleep(time.Duration(1) * time.Second)
+
+	set := MakeSetClient(context.Background(), port)
+	get := MakeGetClient(context.Background(), port)
+
+	fmt.Println("set key1:abc")
+	set(context.Background(), KeyValueStruct{Key: "key1", Value: "abc"})
+
+	fmt.Println("set key2:123")
+	set(context.Background(), KeyValueStruct{Key: "key2", Value: "123"})
+
+	response, err := get(context.Background(), KeyValueStruct{Key: "key2", Value: "0"})
+	if err != nil {
+		return
+	}
+	resp := response.(KeyValueStruct)
+	fmt.Println("get : ", resp.Key, resp.Value)
+
+	response, err = get(context.Background(), KeyValueStruct{Key: "key1", Value: "0"})
+	if err != nil {
+		return
+	}
+	resp = response.(KeyValueStruct)
+	fmt.Println("get : ", resp.Key, resp.Value)
+}
+
+func RunProxy(port string, ports []string) {
+	var service KeyValueService
+	service = &(MapService{})
+	var proxyUrls []string
+	for _, port := range ports {
+		url := "http://localhost" + port
+		proxyUrls = append(proxyUrls, url)
+	}
+
+	service = NewProxy(context.Background(), proxyUrls)(service)
+
+	setHandler := httptransport.NewServer(
+		makeSetEndpoint(service),
+		decodeReq,
+		encodeResp,
+	)
+
+	getHandler := httptransport.NewServer(
+		makeGetEndpoint(service),
+		decodeReq,
+		encodeResp,
+	)
+
+	mux := http.NewServeMux()
+	mux.Handle("/set", setHandler)
+	mux.Handle("/get", getHandler)
+	http.ListenAndServe(port, mux)
+}
+func main() {
+	go RunClient(":8000")
+	ports := []string{
+		":8080",
+		":8081",
+		":8082",
+	}
+	for _, port := range ports {
+		go RunService(port)
+	}
+
+	RunProxy(":8000", ports)
+}
