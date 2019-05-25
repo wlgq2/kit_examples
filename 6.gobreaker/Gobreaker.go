fe47a1ee75ffd37ff7dc06108cd20d5dd280c601
wlgq2 <orcaer@yeah.net>
2019-05-25 16:05:27 +0800

    update gobreaker.

--- 6.gobreaker/Gobreaker.go
+++ 6.gobreaker/Gobreaker.go
@@ -20,7 +20,14 @@ import (
 
 func newGobreaker(endpoint endpoint.Endpoint, qps int) endpoint.Endpoint {
 
-	middleware := circuitbreaker.Gobreaker(gobreaker.NewCircuitBreaker(gobreaker.Settings{}))
+	middleware := circuitbreaker.Gobreaker(gobreaker.NewCircuitBreaker(gobreaker.Settings{
+		Name:        "my gobreaker",
+		MaxRequests: 100,             //半开状态
+		Interval:    time.Second,     //计数周期
+		Timeout:     time.Second * 2, //进入半开状态周期
+		//ReadyToTrip   func(counts Counts) bool   打开条件，默认5次出错
+		//OnStateChange func(name string, from State, to State)   //状态切换触发
+	}))
 	rst := middleware(endpoint)
 	rst = ratelimit.NewErroringLimiter(rate.NewLimiter(rate.Every(time.Second), qps))(rst)
 	return rst
